e016a33338faae0925efac09b0ed74a6ccd740e4
tor.erlend95@gmail.com <tor.erlend95@gmail.com>
2020-04-22 18:27:02 +0100

    fixed parameterization of NegativeBinomial2

--- src/utils.jl
+++ src/utils.jl
@@ -1,22 +1,39 @@
 using Distributions
 
 """
-    NegativeBinomial2(μ, σ²)
+    NegativeBinomial2(μ, ϕ)
 
 Mean-variance parameterization of `NegativeBinomial`.
 
+## Derivation
+`NegativeBinomial` from `Distributions.jl` is parameterized following [1]. With the parameterization in [2], we can solve
+for `r` (`n` in [1]) and `p` by matching the mean and the variance given in `μ` and `ϕ`.
+
+We have the following two equations
+
+(1) μ = r (1 - p) / p
+(2) μ + μ^2 / ϕ = r (1 - p) / p^2
+
+Substituting (1) into the RHS of (2):
+  μ + (μ^2 / ϕ) = μ / p
+⟹ 1 + (μ / ϕ) = 1 / p
+⟹ p = 1 / (1 + μ / ϕ)
+⟹ p = (1 / (1 + μ / ϕ)
+
+Then in (1) we have
+  μ = r (1 - (1 / 1 + μ / ϕ)) * (1 + μ / ϕ)
+⟹ μ = r ((1 + μ / ϕ) - 1)
+⟹ r = ϕ
+
+Hence, the resulting map is `(μ, ϕ) ↦ NegativeBinomial(ϕ, 1 / (1 + μ / ϕ))`.
+
 ## References
-- https://en.wikipedia.org/wiki/Negative_binomial_distribution#Alternative_formulations
-- https://mc-stan.org/docs/2_20/functions-reference/nbalt.html
+[1] https://reference.wolfram.com/language/ref/NegativeBinomialDistribution.html
+[2] https://mc-stan.org/docs/2_20/functions-reference/nbalt.html
 """
 function NegativeBinomial2(μ, ϕ)
-    p = 1 / (1 + ϕ / μ)
+    p = 1 / (1 + μ / ϕ)
     r = ϕ
-
-    # # TODO remove
-    # (p ≤ .0 || p > 1.) && @info "r < 0: $(ReverseDiff.value(r)) for inputs" ReverseDiff.value(μ) ReverseDiff.value(ϕ)
-
-    # @info "NegativeBinomial2" ReverseDiff.value(σ²) ReverseDiff.value(μ) ReverseDiff.value(ϕ) ReverseDiff.value(p) ReverseDiff.value(r)
 
     return NegativeBinomial(r, p)
 end
