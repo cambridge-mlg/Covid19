3c8e8f38965a8da28084ed9e5fd5d76abaff892f
tor.erlend95@gmail.com <tor.erlend95@gmail.com>
2020-04-27 23:02:04 +0100

    refractored the bugfix a bit

--- src/Covid19.jl
+++ src/Covid19.jl
@@ -4,6 +4,13 @@ export ImperialReport13,
     NegativeBinomial2,
     GammaMeanCv
 
+# Fix for stuff
+using Distributions, Random, DistributionsAD
+
+function Distributions.rand(rng::Random.AbstractRNG, d::DistributionsAD.FillVectorOfUnivariate)
+    return rand(rng, d.v.value, length(d))
+end
+
 include("utils.jl")
 
 # Different related reports
