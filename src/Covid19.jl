3a89e7dfe364c3da56a8bb05c3769be7a3340b98
tor.erlend95@gmail.com <tor.erlend95@gmail.com>
2020-04-28 15:07:25 +0100

    cleaned up some functionality from notebooks

--- src/Covid19.jl
+++ src/Covid19.jl
@@ -2,7 +2,11 @@ module Covid19
 
 export ImperialReport13,
     NegativeBinomial2,
-    GammaMeanCv
+    GammaMeanCv,
+    generated_quantities,
+    vectup2tupvec,
+    plot_confidence_timeseries,
+    plot_confidence_timeseries!
 
 # Fix for stuff
 using Distributions, Random, DistributionsAD
@@ -11,7 +15,9 @@ function Distributions.rand(rng::Random.AbstractRNG, d::DistributionsAD.FillVect
     return rand(rng, d.v.value, length(d))
 end
 
-include("utils.jl")
+include("utils.jl")           # <= stuff that might also be included by sub-modules
+include("utils_overloads.jl") # <= overloads of methods which we DON'T want to include in sub-modules so we don't overload multiple times
+include("visualization.jl")   # <= visualization stuff
 
 # Different related reports
 include("imperial-report13/ImperialReport13.jl")
--- src/utils.jl
+++ src/utils.jl
@@ -1,4 +1,4 @@
-using Distributions
+using Turing, Distributions
 
 """
     NegativeBinomial2(μ, ϕ)
@@ -51,4 +51,43 @@ function GammaMeanCv(mean, cv)
     k = cv^(-2)
     θ = mean / k
     return Gamma(k, θ)
+end
+
+"""
+    generated_quantities(m::Turing.Model, c::Turing.MCMCChains.Chains)
+
+Executes `m` for each of the samples in `c` and returns an array of the values returned by the `m` for each sample.
+
+## Examples
+Often you might have additional quantities computed inside the model that you want to inspect, e.g.
+```julia
+@model demo(x) = begin
+    # sample and observe
+    θ ~ Prior()
+    x ~ Likelihood()
+
+    return interesting_quantity(θ, x)
+end
+
+m = demo(data)
+chain = sample(m, alg, n)
+
+# To inspect the `interesting_quantity(θ, x)` where `θ` is replaced by samples from the posterior/`chain`:
+generated_quantities(m, chain)
+```
+"""
+function generated_quantities(m::Turing.Model, c::Turing.MCMCChains.Chains)
+    varinfo = Turing.DynamicPPL.VarInfo(m)
+
+    map(1:length(c)) do i
+        Turing.DynamicPPL._setval!(varinfo, c[i])
+        m(varinfo)
+    end
+end
+
+"Converts a vector of tuples to a tuple of vectors."
+function vectup2tupvec(ts::AbstractVector{<:Tuple})
+    k = length(first(ts))
+    
+    return tuple([[t[i] for t in ts] for i = 1:k]...)
 end
