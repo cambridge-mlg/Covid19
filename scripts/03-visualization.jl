cef54f21adfe4b481298d401f1d21a4ccb047278
tor.erlend95@gmail.com <tor.erlend95@gmail.com>
2020-05-03 19:21:16 +0200

    updated visualization script

--- scripts/03-visualization.jl
+++ scripts/03-visualization.jl
@@ -31,6 +31,9 @@ Pkg.status()
 
 import DrWatson: datadir
 
+outdir() = projectdir("out")
+outdir(args...) = projectdir("out", args...)
+
 datadir() = projectdir("data", "imperial-report13")
 datadir(s...) = projectdir("data", "imperial-report13", s...)
 
@@ -187,7 +190,7 @@ uk_index = findfirst(==("United_Kingdom"), countries)
     # since this model does not include any notion of migration across borders.
     # => might has well wrap it in a `@threads` to perform the computation in parallel.
     @threads for m = 1:num_countries
-        # country-specific parameters
+        # Country-specific parameters
         π_m = π[m]
         pop_m = population[m]
         expected_daily_cases_m = expected_daily_cases[m]
@@ -206,13 +209,16 @@ uk_index = findfirst(==("United_Kingdom"), countries)
         xs = covariates[m][1:last_time_step, :] # extract covariates for the wanted time-steps and country `m`
         Rt_m .= μ[m] * exp.(xs * (-α) + (- lockdown[m]) * xs[:, lockdown_index])
     
-        # adjusts for portion of pop that are susceptible
+        # Adjusts for portion of pop that are susceptible
         Rt_adj_m[1:num_impute] .= (max.(pop_m .- cases_pred_m[1:num_impute], zero(cases_pred_m[1])) ./ pop_m) .* Rt_m[1:num_impute]
     
         for t = (num_impute + 1):last_time_step
+            # Update cumulative cases
             cases_pred_m[t] = cases_pred_m[t - 1] + expected_daily_cases_m[t - 1]
     
-            Rt_adj_m[t] = (max(pop_m - cases_pred_m[t], zero(cases_pred_m[t])) / pop_m) * Rt_m[t] # adjusts for portion of pop that are susceptible
+            # Adjusts for portion of pop that are susceptible
+            Rt_adj_m[t] = (max(pop_m - cases_pred_m[t], zero(cases_pred_m[t])) / pop_m) * Rt_m[t]
+    
             expected_daily_cases_m[t] = Rt_adj_m[t] * sum(expected_daily_cases_m[τ] * serial_intervals[t - τ] for τ = 1:(t - 1))
         end
     
@@ -299,22 +305,21 @@ function country_prediction_plot(country_idx, predictions_country::AbstractMatri
     daily_cases = data.cases[country_idx]
     
     p1 = plot(; xaxis = false, legend = :topleft)
-    bar!(preproc(daily_deaths), label="$(country_name)")
-    title!("Observed daily deaths")
+    bar!(preproc(daily_deaths), label="Observed daily deaths")
+    title!(replace(country_name, "_" => " "))
     vline!([data.epidemic_start[country_idx]], label="epidemic start", linewidth=2)
     vline!([num_observed_days], label="end of observations", linewidth=2)
     xlims!(0, num_total_days)
 
     p2 = plot(; legend = :topleft, xaxis=false)
-    plot_confidence_timeseries!(p2, preproc(e_deaths_country); label = "$(country_name)")
-    title!("Expected daily deaths (pred)")
-    bar!(preproc(daily_deaths), label="$(country_name) (observed)", alpha=0.5)
+    plot_confidence_timeseries!(p2, preproc(e_deaths_country); label = "Expected daily deaths")
+    # title!("Expected daily deaths (pred)")
+    bar!(preproc(daily_deaths), label="Recorded daily deaths (observed)", alpha=0.5)
 
     p3 = plot(; legend = :bottomleft, xaxis=false)
     plot_confidence_timeseries!(p3, Rt_country; no_label = true)
     for (c_idx, c_time) in enumerate(findfirst.(==(1), eachcol(data.covariates[country_idx])))
         if c_time !== nothing
-            # c_name = names(covariates)[2:end][c_idx]
             c_name = names_covariates[c_idx]
             if (c_name != "any")
                 # Don't add the "any intervention" stuff
@@ -327,25 +332,22 @@ function country_prediction_plot(country_idx, predictions_country::AbstractMatri
     lq, hq = (eachrow(hcat(qs...))..., )
     ylims!(0, maximum(hq) + 0.1)
 
-    # p3 = bar(replace(data.cases[country_idx], missing => -1.), label="$(country_name)")
-    # title!("Daily cases")
-
     p4 = plot(; legend = :topleft, xaxis=false)
-    plot_confidence_timeseries!(p4, preproc(predictions_country); label = "$(country_name)")
-    title!("Expected daily cases (pred)")
-    bar!(preproc(daily_cases), label="$(country_name) (observed)", alpha=0.5)
+    plot_confidence_timeseries!(p4, preproc(predictions_country); label = "Expected daily cases")
+    # title!("Expected daily cases (pred)")
+    bar!(preproc(daily_cases), label="Recorded daily cases (observed)", alpha=0.5)
 
     vals = preproc(cumsum(e_deaths_country; dims = 1))
     p5 = plot(; legend = :topleft, xaxis=false)
-    plot_confidence_timeseries!(p5, vals; label = "$(country_name)")
-    plot!(preproc(cumsum(daily_deaths)), label="observed/recorded", color=:red)
-    title!("Expected deaths (pred)")
+    plot_confidence_timeseries!(p5, vals; label = "Expected deaths")
+    plot!(preproc(cumsum(daily_deaths)), label="Recorded deaths (observed)", color=:red)
+    # title!("Expected deaths (pred)")
 
     vals = preproc(cumsum(predictions_country; dims = 1))
     p6 = plot(; legend = :topleft)
-    plot_confidence_timeseries!(p6, vals; label = "$(country_name)")
-    plot!(preproc(daily_cases), label="observed/recorded", color=:red)
-    title!("Expected cases (pred)")
+    plot_confidence_timeseries!(p6, vals; label = "Expected cases")
+    plot!(preproc(daily_cases), label="Recorded cases (observed)", color=:red)
+    # title!("Expected cases (pred)")
 
     p = plot(p1, p3, p2, p4, p5, p6, layout=(6, 1), size=(900, 1200), sharex=true)
     xticks!(1:3:num_total_days, date_strings[1:3:end], xrotation=45)
@@ -383,15 +385,204 @@ parameters = (
 
 chains_posterior = sample(m_no_pred, NUTS(parameters.warmup, 0.95, 10), parameters.steps + parameters.warmup)
 
+using PyCall
+
+using PyCall: pyimport
+pystan = pyimport("pystan");
+
+model_str = raw"""
+data {
+  int <lower=1> M; // number of countries
+  int <lower=1> P; // number of covariates
+  int <lower=1> N0; // number of days for which to impute infections
+  int<lower=1> N[M]; // days of observed data for country m. each entry must be <= N2
+  int<lower=1> N2; // days of observed data + # of days to forecast
+  int cases[N2,M]; // reported cases
+  int deaths[N2, M]; // reported deaths -- the rows with i > N contain -1 and should be ignored
+  matrix[N2, M] f; // h * s
+  matrix[N2, P] X[M]; // features matrix
+  int EpidemicStart[M];
+  real pop[M];
+  real SI[N2]; // fixed pre-calculated SI using emprical data from Neil
+}
+
+transformed data {
+  vector[N2] SI_rev; // SI in reverse order
+  vector[N2] f_rev[M]; // f in reversed order
+  
+  for(i in 1:N2)
+    SI_rev[i] = SI[N2-i+1];
+    
+  for(m in 1:M){
+    for(i in 1:N2) {
+     f_rev[m, i] = f[N2-i+1,m];
+    }
+  }
+}
+
+
+parameters {
+  real<lower=0> mu[M]; // intercept for Rt
+  real<lower=0> alpha_hier[P]; // sudo parameter for the hier term for alpha
+  real<lower=0> gamma;
+  vector[M] lockdown;
+  real<lower=0> kappa;
+  real<lower=0> y[M];
+  real<lower=0> phi;
+  real<lower=0> tau;
+  real <lower=0> ifr_noise[M];
+}
+
+transformed parameters {
+    vector[P] alpha;
+    matrix[N2, M] prediction = rep_matrix(0,N2,M);
+    matrix[N2, M] E_deaths  = rep_matrix(0,N2,M);
+    matrix[N2, M] Rt = rep_matrix(0,N2,M);
+    matrix[N2, M] Rt_adj = Rt;
+    
+    {
+      matrix[N2,M] cumm_sum = rep_matrix(0,N2,M);
+      for(i in 1:P){
+        alpha[i] = alpha_hier[i] - ( log(1.05) / 6.0 );
+      }
+      for (m in 1:M){
+        prediction[1:N0,m] = rep_vector(y[m],N0); // learn the number of cases in the first N0 days
+        cumm_sum[2:N0,m] = cumulative_sum(prediction[2:N0,m]);
+        
+        Rt[,m] = mu[m] * exp(-X[m] * alpha - X[m][,5] * lockdown[m]);
+        Rt_adj[1:N0,m] = Rt[1:N0,m];
+        for (i in (N0+1):N2) {
+          real convolution = dot_product(sub_col(prediction, 1, m, i-1), tail(SI_rev, i-1));
+          cumm_sum[i,m] = cumm_sum[i-1,m] + prediction[i-1,m];
+          Rt_adj[i,m] = ((pop[m]-cumm_sum[i,m]) / pop[m]) * Rt[i,m];
+          prediction[i, m] = Rt_adj[i,m] * convolution;
+        }
+        E_deaths[1, m]= 1e-15 * prediction[1,m];
+        for (i in 2:N2){
+          E_deaths[i,m] = ifr_noise[m] * dot_product(sub_col(prediction, 1, m, i-1), tail(f_rev[m], i-1));
+        }
+      }
+    }
+}
+model {
+  tau ~ exponential(0.03);
+  for (m in 1:M){
+      y[m] ~ exponential(1/tau);
+  }
+  gamma ~ normal(0,.2);
+  lockdown ~ normal(0,gamma);
+  phi ~ normal(0,5);
+  kappa ~ normal(0,0.5);
+  mu ~ normal(3.28, kappa); // citation: https://academic.oup.com/jtm/article/27/2/taaa021/5735319
+  alpha_hier ~ gamma(.1667,1);
+  ifr_noise ~ normal(1,0.1);
+  for(m in 1:M){
+    deaths[EpidemicStart[m]:N[m], m] ~ neg_binomial_2(E_deaths[EpidemicStart[m]:N[m], m], phi);
+   }
+}
+
+generated quantities {
+    matrix[N2, M] prediction0 = rep_matrix(0,N2,M);
+    matrix[N2, M] E_deaths0  = rep_matrix(0,N2,M);
+    
+    {
+      matrix[N2,M] cumm_sum0 = rep_matrix(0,N2,M);
+      for (m in 1:M){
+         for (i in 2:N0){
+          cumm_sum0[i,m] = cumm_sum0[i-1,m] + y[m]; 
+        }
+        prediction0[1:N0,m] = rep_vector(y[m],N0); 
+        for (i in (N0+1):N2) {
+          real convolution0 = dot_product(sub_col(prediction0, 1, m, i-1), tail(SI_rev, i-1));
+          cumm_sum0[i,m] = cumm_sum0[i-1,m] + prediction0[i-1,m];
+          prediction0[i, m] = ((pop[m]-cumm_sum0[i,m]) / pop[m]) * mu[m] * convolution0;
+        }
+        E_deaths0[1, m]= 1e-15 * prediction0[1,m];
+        for (i in 2:N2){
+          E_deaths0[i,m] = ifr_noise[m] * dot_product(sub_col(prediction0, 1, m, i-1), tail(f_rev[m], i-1));
+        }
+      }
+    }
+}
+"""
+
+d = Dict([(k, rdata[k]) for k in keys(rdata)]); # `values(df)` and `keys(df)` have different ordering so DON'T do `Dict(keys(df), values(df))`
+
+sm = pystan.StanModel(model_code=model_str)
+
+fit_stan(n_iters=300, warmup=100) = sm.sampling(
+    data=d, iter=n_iters, chains=1, warmup=warmup, algorithm="NUTS", 
+    control=Dict(
+        "adapt_delta" => 0.95,
+        "max_treedepth" => 10
+    )
+)
+f = fit_stan(parameters.steps + parameters.warmup, parameters.warmup)
+
+la = f.extract(permuted=true)
+
+using Serialization
+
+stan_chain_fname = first([s for s in readdir(outdir()) if occursin("stan", s)])
+la = open(io -> deserialize(io), outdir(stan_chain_fname), "r")
+
+rename!(
+    la,
+    "alpha" => "α",
+    "alpha_hier" => "α_hier",
+    "kappa" => "κ",
+    "gamma" => "γ",
+    "mu" => "μ",
+    "phi" => "ϕ",
+    "tau" => "τ"
+)
+
+# Extract a subset of the variables, since we don't want everything in a `Chains` object
+la_subset = Dict(
+    k => la[k] for k in 
+    ["y", "κ", "α_hier", "ϕ", "τ", "ifr_noise", "μ", "γ", "lockdown"]
+)
+
+function MCMCChains._cat(::Val{3}, c1::Chains, args::Chains...)
+    # check inputs
+    rng = range(c1)
+    # (OR not, he he he)
+    # all(c -> range(c) == rng, args) || throw(ArgumentError("chain ranges differ"))
+    nms = names(c1)
+    all(c -> names(c) == nms, args) || throw(ArgumentError("chain names differ"))
+
+    # concatenate all chains
+    data = mapreduce(c -> c.value.data, (x, y) -> cat(x, y; dims = 3), args;
+                     init = c1.value.data)
+    value = MCMCChains.AxisArray(data; iter = rng, var = nms, chain = 1:size(data, 3))
+
+    return Chains(value, missing, c1.name_map, c1.info)
+end
+
+stan_chain = Chains(la_subset); # <= results in all chains being concatenated together so we need to manually "separate" them
+
+steps_per_chain = parameters.steps
+num_chains = Int(length(stan_chain) // steps_per_chain)
+
+stan_chains = [stan_chain[1 + (i - 1) * steps_per_chain:i * steps_per_chain] for i = 1:num_chains];
+stan_chains = chainscat(stan_chains...);
+stan_chains = stan_chains[1:3:end] # thin
+
 filenames = [
-    relpath(projectdir("out", s)) for s in readdir(projectdir("out"))
+    relpath(outdir(s)) for s in readdir(outdir())
     if occursin(savename(parameters), s) && occursin("seed", s)
+]
+filenames = [
+    "out/chains_model=imperial-report13-v2-vectorized-non-predict-6-threads-updated-full-truncation_seed=1_steps=3000_warmup=1000_with_lockdown=true.jls",
+    "out/chains_model=imperial-report13-v2-vectorized-non-predict-6-threads-updated-full-truncation_seed=2_steps=3000_warmup=1000_with_lockdown=true.jls",
+    "out/chains_model=imperial-report13-v2-vectorized-non-predict-6-threads-updated-full-truncation_seed=3_steps=3000_warmup=1000_with_lockdown=true.jls",
+    "out/chains_model=imperial-report13-v2-vectorized-non-predict-6-threads-updated-full-truncation_seed=4_steps=3000_warmup=1000_with_lockdown=true.jls",
 ]
 length(filenames)
 
-chains_posterior_vec = [read(fname, Chains) for fname in filenames]; # read the different chains
-chains_posterior = chainscat(chains_posterior_vec...); # concatenate them
-chains_posterior = chains_posterior[1:3:end] # <= thin so we're left with 1000 samples
+chains_posterior_vec = [read(fname, Chains) for fname in filenames]; # Read the different chains
+chains_posterior = chainscat(chains_posterior_vec...); # Concatenate them
+chains_posterior = chains_posterior[1:3:end] # <= Thin so we're left with 1000 samples
 
 plot(chains_posterior[[:κ, :ϕ, :τ]]; α = .5, linewidth=1.5)
 
@@ -430,15 +621,13 @@ function zero_covariates(xs::AbstractMatrix{<:Real}, names_covariates; remove=[]
     @assert (isempty(remove) || isempty(keep)) "only `remove` or `keep` can be non-empty"
     
     if isempty(keep)
-        return hcat([
-            (names_covariates[i] ∈ remove ? zeros(eltype(c), length(c)) : c) 
-            for (i, c) in enumerate(eachcol(xs))
-        ]...)
+        return mapreduce(hcat, enumerate(eachcol(xs))) do (i, c)
+            (names_covariates[i] ∈ remove ? zeros(eltype(c), length(c)) : c)
+        end
     else
-        return hcat([
-        (names_covariates[i] ∈ keep ? c : zeros(eltype(c), length(c))) 
-        for (i, c) in enumerate(eachcol(xs))
-        ]...)
+        return mapreduce(hcat, enumerate(eachcol(xs))) do (i, c)
+            (names_covariates[i] ∈ keep ? c : zeros(eltype(c), length(c))) 
+        end
     end
 end
 
